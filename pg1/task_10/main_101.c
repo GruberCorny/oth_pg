bce076bba0cb90318e0a1738fbd38c0b67d08531
GruberCorny <co.postfach@gmail.com>
2022-12-29 11:14:04 +0100

    added tasks and delte ds store

---
+++ pg1/task_10/main_101.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NAME_LEN 50
+
+typedef struct
+{
+    char name[MAX_NAME_LEN];
+    int sallery;
+    int identification_number;
+} Employee;
+
+
+
+int main()
+{
+
+    return 0;
+}
